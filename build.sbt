d09c3c026d7a5677355fe6dd33eb4c5f127642e6
seriar <iarovyi.sergii@gmail.com>
2022-02-05 14:48:19 +0200

    Initial code

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "test-sonarcloud"
+
+version := "0.1"
+
+scalaVersion := "2.13.8"
---
+++ src/main/scala/App.scala
@@ -0,0 +1,33 @@
+object App {
+
+  def main(args: Array[String]): Unit = {
+    println("Starting...")
+    ArgsHandler.createApp(args) match {
+      case Some(app) => app.start()
+      case None => println("Error!")
+    }
+
+  }
+}
+
+class App(user: String, repo: String) {
+  def start(): Unit = {
+    var exit: Boolean = false
+    while (!exit) {
+      val input = scala.io.StdIn.readLine("Provide issue id:")
+      if (input == "q") {
+        exit = true
+      } else {
+        try {
+          println(createGithubLink(input.toInt))
+        } catch {
+          case _: Exception => println("use `q` or provide issue id to create a link")
+        }
+      }
+    }
+  }
+
+  def createGithubLink(id: Int): String = {
+    s"[$repo-$id](https://github.com/$user/$repo/issues/$id)"
+  }
+}
